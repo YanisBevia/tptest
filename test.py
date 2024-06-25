5b662b2c89e02ad3858b0925ffb023d255275163
YanisBevia <yanisbevia@hotmail.com>
2024-06-25 13:45:49 +0200

    phase 1 non valid test

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 def Add(numbers: str) -> int:
     if numbers == "":
         return 0
-    return int(numbers)
+    elif "," in numbers:
+        num1, num2 = numbers.split(",")
+        return int(num1) + int(num2)
+    else:
+        return int(numbers)
 
 # Tests
 assert Add("") == 0
 assert Add("1") == 1
 assert Add("2") == 2
+assert Add("1,2") == 3
+assert Add("10,20") == 25
